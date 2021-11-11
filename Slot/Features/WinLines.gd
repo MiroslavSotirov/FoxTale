40ef4a253e45e85f3675bdfdceadeebf71a7184a
Stefano <stefano@elysiumstudios.se>
2021-11-11 14:38:38 +0200

    Progress

--- Slot/Features/WinLines.gd
+++ Slot/Features/WinLines.gd
@@ -32,21 +32,25 @@ func show_lines(windata):
 	Globals.singletons["WinlinesOverlap"].tween(0,1,0.5);
 	for win in windata:
 		if(win.has("winline") && win["winline"] < 0): continue;
-		show_line(win["symbol_positions"]);
+		show_line(win["symbol_positions"], int(win["winline"]));
 		yield(get_tree().create_timer(0.1), "timeout")
 	emit_signal("ShowEnd");
 		
-func show_line(positions):
-	var global_positions = [];
-		
+func show_line(positions, lineid):
+	var line_positions = [];
+	
+	for pos in lines[lineid]:
+		var y = int(pos)%Globals.visible_tiles_count;
+		var x = floor(pos/Globals.visible_tiles_count);		
+		var tile = Globals.singletons["Slot"].get_tile_at(x,y);
+		line_positions.append(tile.global_position);
+	
 	for pos in positions:
 		#TODO: dynamic
 		var y = int(pos)%Globals.visible_tiles_count;
 		var x = floor(pos/Globals.visible_tiles_count);		
 		var tile = Globals.singletons["Slot"].get_tile_at(x,y);
-		if(shown_tiles.has(pos)): 
-			global_positions.append(tile.global_position);
-		else:
+		if(!shown_tiles.has(pos)): 
 			shown_tiles.append(pos);
 			var wintile = tile_scene.instance();		
 			$TilesContainer.add_child(wintile);
@@ -54,12 +58,11 @@ func show_line(positions):
 			wintile.tileY = tile.tileIndex;
 			wintile.set_id(tile.data.id);
 			wintile.global_position = tile.global_position;
-			global_positions.append(tile.global_position);
 		
 	var winline = line_scene.instance();
 	$LinesContainer.add_child(winline);
-	winline.global_position = global_positions[0];
-	winline.positions = global_positions;
+	winline.global_position = line_positions[0];
+	winline.positions = line_positions;
 	winline.init();
 	
 func hide_lines():
