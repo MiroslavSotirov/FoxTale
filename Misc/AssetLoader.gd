d9507795c98b0b6bff6c9b3c91a9bddcac817732
Georgi Lalev <glalev@elysiumstudios.se>
2022-03-22 15:50:38 +0200

    Add smaller bitmap font images

--- Misc/AssetLoader.gd
+++ Misc/AssetLoader.gd
@@ -10,12 +10,28 @@ signal lang_downloaded (lang);
 func _ready():
 	Globals.register_singleton("AssetLoader", self);
 
+func dir_contents(path):
+	var dir = Directory.new()
+	print('looking at: ', path)
+	if dir.open(path) == OK:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if dir.current_is_dir():
+				print("Found directory: " + file_name)
+			else:
+				print("Found file: " + file_name)
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
+
 func download_language(lang):
 	if(JS.enabled):
 		lang = lang.to_upper();
 
 		var path = JS.get_path()+"translations/"+lang+".pck";
 		prints(path);
+		prints("-----------------------------4---------------------------");
 		$HTTPRequest.download_file = "res://"+lang+".pck";
 		$HTTPRequest.request(path);
 		var res = yield($HTTPRequest, "request_completed");
@@ -25,13 +41,29 @@ func download_language(lang):
 			download_language("EN");
 			return;
 			
-		ProjectSettings.load_resource_pack("res://"+lang+".pck")
+#		ProjectSettings.load_resource_pack("res://"+lang+".pck")
+		var wasLoaded = ProjectSettings.load_resource_pack("res://EN.pck")
+		print("was loaded: ", wasLoaded);
 		language_loaded = true;
+		dir_contents('./');
+		dir_contents('./tmp');
+		dir_contents('./home');
+		dir_contents('./proc');
+		dir_contents('./userfs');
+		dir_contents('./http:');
+		
 		emit_signal("lang_downloaded", lang);
+
 		prints("LOADED NEW LANGUAGE ", lang);
 	else:
+		prints("+++++++++++++++++++++++++++++2---------------------------");
 		ProjectSettings.load_resource_pack("res://Translations/export/"+lang+".pck")
 		language_loaded = true;
 		yield(get_tree(), "idle_frame");
 		emit_signal("lang_downloaded", lang);
 		prints("LOADED NEW LANGUAGE ", lang);
+		dir_contents('./');
+		
+	
+
+
